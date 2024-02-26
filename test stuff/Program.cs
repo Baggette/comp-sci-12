c7b483833244054eff2ea3803d70758b4207fed5
baggette <58569691+Baggette@users.noreply.github.com>
2024-02-26 09:31:04 -0600

    dfsfds

---
+++ test stuff/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Program
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string[] colours = { "green", "red", "purple", "blue", "yellow" };
+            for (int i = 0; i < colours.GetLength(0); i++)
+            {
+                Console.WriteLine($"colours! {colours[i]}");
+            }
+        }
+    }
+}
